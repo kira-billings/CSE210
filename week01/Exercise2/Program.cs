f243be0976125eee70d4f00d6c94a391ee1c63d0
kira-billings <dazzlingkirab@gmail.com>
2025-10-29 10:26:56 -0600

    completed exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the score percentage: ");
+        string gradeInput = Console.ReadLine();
+        int gradeScore = int.Parse(gradeInput);
+        string letter = "";
+        if (gradeScore >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeScore >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeScore >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeScore >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (gradeScore >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass but I encourage you to keep trying. You can do it!");
+        }
     }
 }
