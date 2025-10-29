9f5a39f9fe91a28cc7ab050bab6371b81d78ced8
kira-billings <dazzlingkirab@gmail.com>
2025-10-29 12:21:17 -0600

    completed step 2 of exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int number = int.Parse(Console.ReadLine());
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (number != guess);
     }
 }
