99ee0262c45449c7a1bd6943123b0d95fddf7c1f
kira-billings <dazzlingkirab@gmail.com>
2025-10-30 14:47:15 -0600

    compute the sum of numbers in list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string entry;
+        int entryNumber;
+        Console.WriteLine("Enter a list of numbers, type 0 to quit.");
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.Write("Enter number: ");
+            entry = Console.ReadLine();
+            entryNumber = int.Parse(entry);
+            numbers.Add(entryNumber);
+        } while (entryNumber != 0);
+        int sumNumbers = numbers.Sum();
+            // foreach (int number in numbers)
+           
+        Console.WriteLine($"The sum is : {sumNumbers}");
+            // Console.WriteLine($"The average is: {average}");
+            // Console.WriteLine($"The largest number is: {largest}");
+        
+        // for (int i = 0; i < numbers.Count; i++)
+        // {
+        //     Console.WriteLine(numbers[i]);
+        // }    
+        // Console.WriteLine(numbers.Count);
     }
 }
+        
