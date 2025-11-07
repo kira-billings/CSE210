6f7c8d0e4fbe7d97d626a5581333df26266cb070
kira-billings <dazzlingkirab@gmail.com>
2025-11-07 16:00:29 -0700

    complete W02 Fraction assignment

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions f1 = new Fractions();
+        Fractions f2 = new Fractions(3);
+        Fractions f3 = new Fractions(3, 5);
+
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetFractionString());
+       
     }
 }
