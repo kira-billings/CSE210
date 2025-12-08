89609ff8671901881ea4de2de21d5d4d39b41db6
kira-billings <dazzlingkirab@gmail.com>
2025-12-07 20:15:41 -0700

    added comment to program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,5 @@
+// Added two options to list completed goals and to list uncompleted goals to exceed the requirements.
+
 using System;
 using System.IO;
 
@@ -8,11 +10,7 @@ class Program
         Console.WriteLine();
         Console.WriteLine("Welcome to the EternalQuest Project.");
         GoalManager goalManager = new GoalManager();
-
         goalManager.Start();
-
-
         Console.WriteLine("Go and do the things!");
-        
     }
 }
